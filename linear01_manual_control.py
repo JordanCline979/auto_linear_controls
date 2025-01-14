908959323833dee0d7a247654ca69e1c0bca7f84
Rpi Linear Controls <jordanc@ifit.com>
2025-01-14 13:29:00 -0700

    adding input options and input validation on manual control (which is really remote control but manually activated)

--- linear01_manual_control.py
+++ linear01_manual_control.py
@@ -8,3 +8,28 @@ from time import sleep
 parser = argparse.ArgumentParser()
 parser.add_argument('--direction')
 parser.add_argument('--distance')
+parser.add_argument('--speed')
+
+args = parser.parse_args()
+direction = args.direction
+distance = args.distance    # measured in mm
+
+if args.speed != None:  # measured in mm/s; deaults to 50 mm/s
+    speed = args.speed
+else: speed = 50 
+
+if direction == None or distance == None:   # bad usage case, direction & distance must be provided
+    print("Invalid or missing mode; use --direction forward | backward --distance dist_mm"); exit(1)
+if float(distance) > 100.0 or float(distance) < 0:    # bad usage case, max stroke length is 100 mm, min is 0 mm
+    print("Invalid input for distance. Maximum distance is 100mm"); exit(1)
+
+if float(speed) > 50.0 or float(speed) < 1.0: # bad usage case, max speed is 50 mm/s (design constraint) and min speed is 1 mm/s (artificial constraint)
+    print("Invalid input for speed")
+
+# -------------------------------------------- #
+
+#sanity check
+print(f"value for direction is {direction}")
+print(f"value for distance is {float(distance)}")
+print(f"value for speed is {float(speed)}")
+
